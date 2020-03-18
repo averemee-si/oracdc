88fb0e241dc9f644988d1e271678d419f2c64797
alessio <alessio@localhost>
2020-03-18 19:56:36 +0300

    Need Oracle jar's for Sink connector too

--- etc/sink-testapps-wsh.sh
+++ etc/sink-testapps-wsh.sh
@@ -3,6 +3,8 @@ CONNECT_HOME=/kafka/connect
 LIB_HOME=$CONNECT_HOME/lib
 # Add  PGJDBC-NG jar's
 CLASSPATH=$LIB_HOME/HikariCP-3.4.1.jar:$LIB_HOME/netty-transport-native-kqueue-4.1.42.Final.jar:$LIB_HOME/pgjdbc-ng-0.8.3.jar:$LIB_HOME/spy-0.8.3.jar
+# Add Oracle jar's
+CLASSPATH=$LIB_HOME/ojdbc8.jar:$LIB_HOME/ucp.jar:$LIB_HOME/oraclepki.jar:$LIB_HOME/osdt_core.jar:$LIB_HOME/osdt_cert.jar:${CLASSPATH}
 # Add misc jar's
 CLASSPATH=$LIB_HOME/commons-math3-3.6.1.jar:${CLASSPATH}
 export CLASSPATH
