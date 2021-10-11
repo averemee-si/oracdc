da89ebdd2e48df3e68b91bfd12a0af6f69b2cb9f
alessio <alessio@10.0.0.2>
2021-10-11 14:36:13 +0200

    LOB transformation interface and default implementation

---
+++ src/main/java/eu/solutions/a2/cdc/oracle/data/OraCdcLobTransformationsIntf.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2018-present, A2 Rešitve d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package eu.solutions.a2.cdc.oracle.data;
+
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+
+import eu.solutions.a2.cdc.oracle.OraColumn;
+
+/**
+ * 
+ * Interface for LOB/XMLTYPE transformations feature 
+ * 
+ * 
+ * @author averemee
+ *
+ */
+public interface OraCdcLobTransformationsIntf {
+
+	public void transformSchema(final String pdbName, final String tableOwner,
+			final String tableName, final OraColumn lobColumn, final SchemaBuilder valueSchema);
+
+	public void transformData(final String pdbName, final String tableOwner,
+			final String tableName, final OraColumn lobColumn, final byte[] content,
+			final Struct keyStruct, final Struct valueStruct);
+
+}
