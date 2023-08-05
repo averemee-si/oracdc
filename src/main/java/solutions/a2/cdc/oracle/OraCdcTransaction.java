b4339247167e0eb8dc832cb7858b63b3c77c5a4b
averemee <averemee@localhost>
2023-08-05 12:56:51 +0200

    clean OraCdcTransaction interface code

--- src/main/java/solutions/a2/cdc/oracle/OraCdcTransaction.java
+++ src/main/java/solutions/a2/cdc/oracle/OraCdcTransaction.java
@@ -1,10 +1,5 @@
 package solutions.a2.cdc.oracle;
 
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-
-
 /**
  * 
  * @author <a href="mailto:averemee@a2.solutions">Aleksei Veremeev</a>
@@ -14,26 +9,15 @@ public interface OraCdcTransaction {
 
 
 	public void addStatement(final OraCdcLogMinerStatement oraSql);
-	//TODO ???
-//	public void addStatement(final OraCdcLogMinerStatement oraSql, final List<OraCdcLargeObjectHolder> lobs);
 	public boolean getStatement(OraCdcLogMinerStatement oraSql);
-	//TODO ???
-//	public boolean getStatement(OraCdcLogMinerStatement oraSql, List<OraCdcLargeObjectHolder> lobs);
-	//TODO ???
-//	public boolean getLobs(final int lobCount, final List<OraCdcLargeObjectHolder> lobs);
 	public void close();
 	public int length();
 	public int offset();	
-	//TODO ???
-//	public Map<String, Object> attrsAsMap();
 	public String getXid();
 	public long getFirstChange();
 	public long getNextChange();
 	public Long getCommitScn();
 	public void setCommitScn(Long commitScn);
-	//TODO ???
-//	public Path getPath();
 	public long size();
-
 
 }
