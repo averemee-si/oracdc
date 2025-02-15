02bda266462e2eef47864121a7209d69cfe8f712
averemee <averemee@a2.solutions>
2025-02-15 17:11:09 +0100

    rename OraDumpDecoder.toByteArray() to hexToRaw()

--- src/main/java/solutions/a2/cdc/oracle/OraDumpDecoder.java
+++ src/main/java/solutions/a2/cdc/oracle/OraDumpDecoder.java
@@ -188,17 +188,6 @@ public class OraDumpDecoder {
 		}
 	}
 
-	@Deprecated
-	public static byte[] toByteArray(final String hex) {
-		int len = hex.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) +
-									Character.digit(hex.charAt(i+1), 16));
-		}
-		return data;
-	}
-
 	public static byte[] hexToRaw(final String hex) {
 		final int len = hex.length();
 		final byte[] data = new byte[len / 2];
