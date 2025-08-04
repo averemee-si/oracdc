32c9b85b14a1769176102fcc0fef61b0c7c6fced
averemee <averemee@a2.solutions>
2025-08-04 20:32:10 +0200

    required TDE data dictionary privileges

---
+++ sql/redo_miner_tde_cdb.sql
@@ -0,0 +1,39 @@
+REM +-------------------------------------------------------------------------+
+REM |
+REM | Copyright (c) 2018-present, A2 Rešitve d.o.o.
+REM |
+REM | Licensed under the Apache License, Version 2.0 (the "License");
+REM | you may not use this file except in compliance with the License.
+REM | You may obtain a copy of the License at
+REM |
+REM | http://www.apache.org/licenses/LICENSE-2.0
+REM |
+REM | Unless required by applicable law or agreed to in writing, software
+REM | distributed under the License is distributed on an "AS IS" BASIS,
+REM | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM | See the License for the specific language governing permissions and
+REM | limitations under the License.
+REM |
+REM +-------------------------------------------------------------------------+
+REM |
+REM | DESCRIPTION
+REM |  TDE privileges required for Redo Miner
+REM |
+REM |
+REM +========================================================================*+
+
+set verify off;
+
+WHENEVER SQLERROR EXIT FAILURE ROLLBACK;
+WHENEVER OSERROR  EXIT FAILURE ROLLBACK;
+
+define cdcUserName ='&&1';
+define cdcContainer ='&&2';
+
+grant select on SYS.DBA_ENCRYPTED_COLUMNS to &cdcUserName
+    container=&cdcContainer;
+grant select on SYS.ENC$ to &cdcUserName
+    container=&cdcContainer;
+grant select on SYS.V_$ENCRYPTED_TABLESPACES to &cdcUserName
+    container=&cdcContainer;
+
