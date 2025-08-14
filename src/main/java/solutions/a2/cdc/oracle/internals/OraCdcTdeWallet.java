feef60d0a5abe6bd51363d781fe58dd2bedf2476
averemee <averemee@a2.solutions>
2025-08-14 21:49:08 +0200

    enhanced KEK parsing

--- src/main/java/solutions/a2/cdc/oracle/internals/OraCdcTdeWallet.java
+++ src/main/java/solutions/a2/cdc/oracle/internals/OraCdcTdeWallet.java
@@ -17,6 +17,8 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Decoder;
@@ -38,8 +40,11 @@ import org.slf4j.LoggerFactory;
 import oracle.security.pki.OracleSecretStoreException;
 import oracle.security.pki.OracleWallet;
 
+import solutions.a2.oracle.utils.BinaryUtils;
+
 import static javax.crypto.Cipher.SECRET_KEY;
 import static javax.crypto.Cipher.UNWRAP_MODE;
+import static solutions.a2.oracle.jdbc.types.OracleDate.toLocalDateTime;
 import static solutions.a2.oracle.utils.BinaryUtils.rawToHex;
 
 
@@ -51,16 +56,17 @@ import static solutions.a2.oracle.utils.BinaryUtils.rawToHex;
 public class OraCdcTdeWallet {
 
 	private final OracleWallet wallet;
-	private final Map<String, byte[]> secrets;
+	private final Map<String, Kek> secrets;
+	private String masterKeyId = null;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OraCdcTdeWallet.class);
 	private static final String KEY_PREFIX = "ORACLE.SECURITY.DB.ENCRYPTION.";
 
 	OraCdcTdeWallet(String path, String password) throws IOException {
-		this(path, password.toCharArray());
+		this(BinaryUtils.get(true), path, password.toCharArray());
 	}
 
-	OraCdcTdeWallet(String path, char[] password) throws IOException {
+	OraCdcTdeWallet(BinaryUtils bu, String path, char[] password) throws IOException {
 		wallet = new OracleWallet();
 		try {
 			secrets = new HashMap<>();
@@ -73,23 +79,71 @@ public class OraCdcTdeWallet {
 					final String fullKeyName = (String) element;
 					if (Strings.CS.startsWith(fullKeyName, KEY_PREFIX)) {
 						final String key = StringUtils.substringAfter(fullKeyName, KEY_PREFIX);
-						final byte[] value = decoder.decode(new String(wallet.getSecretStore().getSecret(fullKeyName)));
-						secrets.put(
-								key,
-								value);
-						if (LOGGER.isDebugEnabled())
-							LOGGER.debug("Added key with name '{}', short name '{}' and value '{}'", 
-									fullKeyName, key, rawToHex(value));
-					}
+						final String keyValue = new String(wallet.getSecretStore().getSecret(fullKeyName));
+						if (Strings.CS.endsWith(fullKeyName, "MASTERKEY")) {
+							masterKeyId = keyValue;
+							if (LOGGER.isDebugEnabled())
+								LOGGER.debug("Master Key ID set to '{}'", masterKeyId);
+						} else if (!Strings.CS.contains(fullKeyName, "MASTERKEY")) {
+							final byte[] value = decoder.decode(keyValue);
+							if (value[0] != 0) {
+								LOGGER.error(
+										"\n=====================\n" +
+										"Invalid first byte '{}' for key '{}'!" +
+										"\n=====================\n",
+										Byte.toUnsignedInt(value[0]), fullKeyName);
+								throw new IOException("The first byte of the value structure must be 0!");
+							}
+							final int length = Short.toUnsignedInt(bu.getU16(value, 1));
+							if (length != value.length) {
+								LOGGER.error(
+										"\n=====================\n" +
+										"Invalid value length for key '{}'! There are {} bytes in structure, but the length in the struct content is {}" +
+										"\n=====================\n",
+										fullKeyName, Byte.toUnsignedInt(value[0]), value.length, length);
+								throw new IOException("The first byte of the value structure must be 0!");
+							}
+							int pos = 3;
+							final Kek kek = new Kek();
+							while (pos < length) {
+								int type = Byte.toUnsignedInt(value[pos++]);
+								int len = Short.toUnsignedInt(bu.getU16(value, pos));
+								pos += Short.BYTES;
+								if (type == 1 && len == 0x20)
+									kek.key = Arrays.copyOfRange(value, pos, pos + len);
+								else if (type == 3 && len == 0x10)
+									kek.iv = Arrays.copyOfRange(value, pos, pos + len);
+								else if (type == 5 && len == 0x07) {
+									try {
+										kek.ts = toLocalDateTime(value, pos);
+									} catch (SQLException sqle) {
+										throw new IOException(sqle);
+									}
+								} else {
+									LOGGER.error(
+											"\n=====================\n" +
+											"Unable to parse Oracle Wallet KEK struct with type '{}' and length '{}'!\n" +
+											"Please send this information to oracle@a2.solutions" +
+											"\n=====================\n",
+											type, len);
+									throw new IOException("Unable to parse Oracle Wallet data!");
+								}
+								pos += len;
+							}
+							secrets.put(key, kek);
+							if (LOGGER.isDebugEnabled())
+								LOGGER.debug("Added KEK with name '{}', short name '{}', created on '{}'", 
+										fullKeyName, key, kek.ts);
+						} else {
+							if (LOGGER.isDebugEnabled())
+								LOGGER.debug("Skipping key {}", fullKeyName);
+						}
+					} 
 				}
 			}
 		} catch (OracleSecretStoreException osse) {
 			throw new IOException(osse);
 		}
-	}
-
-	Map<String, byte[]> masterKeys() {
-		return secrets;
 	}
 
 	byte[] decryptDataKey(final String masterKeyId, final String encDataKey) throws IOException {
@@ -101,8 +155,8 @@ public class OraCdcTdeWallet {
 	}
 
 	byte[] decryptDataKey(final String masterKeyId, final byte[] encDataKey, boolean tbsKey) throws IOException {
-		final byte[] masterBytes = secrets.get(masterKeyId);
-		if (masterBytes == null) {
+		final Kek kek = secrets.get(masterKeyId);
+		if (kek == null) {
 			LOGGER.error(
 					"\n=====================\n" +
 					"No data found for master key id '{}'!" +
@@ -120,14 +174,12 @@ public class OraCdcTdeWallet {
 		}
 		try {
 			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-			final byte[] secretBytes = Arrays.copyOfRange(masterBytes, 0x6, 0x26);
-			final byte[] ivBytes = Arrays.copyOfRange(masterBytes, 0x29, 0x39);
-			final SecretKeySpec masterKey = new SecretKeySpec(secretBytes, "AES");
-			final IvParameterSpec iv = new IvParameterSpec(ivBytes);
+			final SecretKeySpec masterKey = new SecretKeySpec(kek.key, "AES");
+			final IvParameterSpec iv = new IvParameterSpec(kek.iv);
 			cipher.init(UNWRAP_MODE, masterKey, iv);
 			if (LOGGER.isDebugEnabled())
-				LOGGER.debug("Decrypting data key {} using master key id '{}'.\n\tSecret key='{}', iv='{}'",
-						rawToHex(encDataKey), masterKeyId, rawToHex(secretBytes), rawToHex(ivBytes));
+				LOGGER.debug("Decrypting DEK {} using KEK id '{}'.\n\tSecret key='{}', iv='{}'",
+						rawToHex(encDataKey), masterKeyId, rawToHex(kek.key), rawToHex(kek.iv));
 			return cipher.unwrap(tbsKey
 					? encDataKey
 					: Arrays.copyOfRange(encDataKey, 1, encDataKey.length),
@@ -135,6 +187,12 @@ public class OraCdcTdeWallet {
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
 			throw new IOException(e);
 		}
+	}
+
+	private static class Kek {
+		byte[] key;
+		byte[] iv;
+		LocalDateTime ts;
 	}
 
 }
