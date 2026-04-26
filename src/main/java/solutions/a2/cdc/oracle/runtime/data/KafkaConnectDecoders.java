ca440b12ac539f8a6f2faa658dc0c043a9774845
averemee <averemee@a2.solutions>
2026-04-26 13:07:19 +0200

    move charset mapping defs from Decoders to RdbmsCharsetMapping class

--- src/main/java/solutions/a2/cdc/oracle/runtime/data/KafkaConnectDecoders.java
+++ src/main/java/solutions/a2/cdc/oracle/runtime/data/KafkaConnectDecoders.java
@@ -42,6 +42,7 @@ import static oracle.sql.NUMBER.toFloat;
 import static solutions.a2.cdc.oracle.data.JdbcTypes.getTypeName;
 import static solutions.a2.cdc.oracle.OraCdcColumn.JAVA_SQL_TYPE_INTERVALDS_STRING;
 import static solutions.a2.cdc.oracle.OraCdcColumn.JAVA_SQL_TYPE_INTERVALYM_STRING;
+import static solutions.a2.cdc.oracle.runtime.data.RdbmsCharsetMapping.ora2JvmCharset;
 import static solutions.a2.oracle.jdbc.types.OracleNumber.toByte;
 import static solutions.a2.oracle.jdbc.types.OracleNumber.toInt;
 import static solutions.a2.oracle.jdbc.types.OracleNumber.toLong;
@@ -52,18 +53,13 @@ import static solutions.a2.oracle.utils.BinaryUtils.rawToHex;
 
 import java.math.RoundingMode;
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
 import java.sql.SQLException;
 import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.agrona.collections.Int2ObjectHashMap;
 import org.apache.kafka.connect.data.Schema;
@@ -117,7 +113,6 @@ import solutions.a2.oracle.jdbc.types.TimestampWithTimeZone;
 public class KafkaConnectDecoders {
 
 	private static final Int2ObjectHashMap<OraCdcDecoder> decoders = new Int2ObjectHashMap<>();
-	private static final Map<String, String> oraToJava = new HashMap<>();
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConnectDecoders.class);
 	
@@ -873,7 +868,7 @@ public class KafkaConnectDecoders {
 		return new OraCdcDecoder() {
 			@Override
 			public Object decode(final byte[] raw, final int off, final int len) throws SQLException {
-				return new String(raw, off, len, charset(oraCharset));
+				return new String(raw, off, len, ora2JvmCharset(oraCharset));
 			}
 		};
 	}
@@ -883,7 +878,7 @@ public class KafkaConnectDecoders {
 			@Override
 			public Object decode(final byte[] raw, final int off, final int len) throws SQLException {
 				final var struct = new Struct(schema);
-				struct.put("V", new String(Arrays.copyOfRange(raw, off, off + len), charset(oraCharset)));
+				struct.put("V", new String(Arrays.copyOfRange(raw, off, off + len), ora2JvmCharset(oraCharset)));
 				return struct;
 			}
 		};
@@ -894,7 +889,7 @@ public class KafkaConnectDecoders {
 			@Override
 			public Object decode(final byte[] raw, final int off, final int len) throws SQLException {
 				final var plaintext = decrypter.decrypt(Arrays.copyOfRange(raw, off, off + len), salted);
-				return new String(plaintext, charset(oraCharset));
+				return new String(plaintext, ora2JvmCharset(oraCharset));
 			}
 		};
 	}
@@ -904,7 +899,7 @@ public class KafkaConnectDecoders {
 			@Override
 			public Object decode(final byte[] raw, final int off, final int len) throws SQLException {
 				final var struct = new Struct(schema);
-				struct.put("V", new String(decrypter.decrypt(Arrays.copyOfRange(raw, off, off + len), salted), charset(oraCharset)));
+				struct.put("V", new String(decrypter.decrypt(Arrays.copyOfRange(raw, off, off + len), salted), ora2JvmCharset(oraCharset)));
 				return struct;
 			}
 		};
@@ -1112,14 +1107,6 @@ public class KafkaConnectDecoders {
 	private static void traceLobInfo(final LobLocator ll, final byte[] ba, final String entity) {
 		LOGGER.trace("Processing {} LID={}, DATALENGTH={}, DATA IN ROW?={}, LOB LOCATOR CONTENT=>{}",
 				entity, ll.lid(), ll.dataLength(), ll.dataInRow(), rawToHex(ba));
-	}
-
-	private static Charset charset(final String oraCharset) {
-		try {
-			return Charset.forName(oraToJava.get(oraCharset));				
-		} catch (UnsupportedCharsetException | IllegalCharsetNameException e) {
-			throw new IllegalArgumentException("Invalid or unsupported Oracle character set: " + oraCharset +  ".", e);
-		}
 	}
 
 	private static boolean toBoolean(final byte[] raw, final int off, final int len) throws SQLException {
@@ -1413,138 +1400,6 @@ public class KafkaConnectDecoders {
 					return transaction.getLob(ll);
 			}
 		});
-
-		oraToJava.put("AL16UTF16", "UTF-16BE");
-		oraToJava.put("AL16UTF16LE", "UTF-16LE");
-		oraToJava.put("AL24UTFFSS", "UTF-8");
-		oraToJava.put("AL32UTF8", "UTF-8");
-		oraToJava.put("AR8ARABICMAC", "MacArabic");
-		oraToJava.put("AR8ARABICMACS", "MacArabic");
-		oraToJava.put("AR8ARABICMACT", "MacArabic");
-		oraToJava.put("AR8EBCDIC420S", "CP420");
-		oraToJava.put("AR8EBCDICX", "CP420");
-		oraToJava.put("AR8ISO8859P6", "ISO-8859-6");
-		oraToJava.put("AR8MSAWIN", "WINDOWS-1256");
-		oraToJava.put("AR8MSWIN1256", "WINDOWS-1256");
-		oraToJava.put("BLT8CP921", "CP921");
-		oraToJava.put("BLT8EBCDIC1112", "CP1112");
-		oraToJava.put("BLT8EBCDIC1112S", "CP1112");
-		oraToJava.put("BLT8ISO8859P13", "ISO-8859-13");
-		oraToJava.put("BLT8MSWIN1257", "WINDOWS-1257");
-		oraToJava.put("BLT8PC775", "CP775");
-		oraToJava.put("CDN8PC863", "CP863");
-		oraToJava.put("CL8EBCDIC1025", "CP1025");
-		oraToJava.put("CL8EBCDIC1025C", "CP1025");
-		oraToJava.put("CL8EBCDIC1025R", "CP1025");
-		oraToJava.put("CL8EBCDIC1025S", "CP1025");
-		oraToJava.put("CL8EBCDIC1025X", "CP1025");
-		oraToJava.put("CL8ISO8859P5", "ISO-8859-5");
-		oraToJava.put("CL8KOI8R", "KOI8-R");
-		oraToJava.put("CL8MACCYRILLIC", "MacCyrillic");
-		oraToJava.put("CL8MACCYRILLICS", "MacCyrillic");
-		oraToJava.put("CL8MSWIN1251", "WINDOWS-1251");
-		oraToJava.put("D8EBCDIC1141", "Cp1141");
-		oraToJava.put("D8EBCDIC273", "CP273");
-		oraToJava.put("DK8EBCDIC1142", "Cp1142");
-		oraToJava.put("DK8EBCDIC277", "IBM277");
-		oraToJava.put("EE8EBCDIC870", "CP870");
-		oraToJava.put("EE8EBCDIC870C", "CP870");
-		oraToJava.put("EE8EBCDIC870S", "CP870");
-		oraToJava.put("EE8ISO8859P2", "ISO-8859-2");
-		oraToJava.put("EE8MACCE", "MacCentralEurope");
-		oraToJava.put("EE8MACCES", "MacCentralEurope");
-		oraToJava.put("EE8MACCROATIAN", "MacCroatian");
-		oraToJava.put("EE8MACCROATIANS", "MacCroatian");
-		oraToJava.put("EE8MSWIN1250", "WINDOWS-1250");
-		oraToJava.put("EE8PC852", "CP852");
-		oraToJava.put("EL8EBCDIC875", "CP875");
-		oraToJava.put("EL8EBCDIC875R", "CP875");
-		oraToJava.put("EL8EBCDIC875S", "CP875");
-		oraToJava.put("EL8ISO8859P7", "ISO-8859-7");
-		oraToJava.put("EL8MSWIN1253", "WINDOWS-1253");
-		oraToJava.put("EL8PC737", "CP737");
-		oraToJava.put("EL8PC869", "CP869");
-		oraToJava.put("F8EBCDIC1147", "Cp1147");
-		oraToJava.put("F8EBCDIC297", "CP297");
-		oraToJava.put("I8EBCDIC1144", "Cp1144");
-		oraToJava.put("I8EBCDIC280", "CP280");
-		oraToJava.put("IS8MACICELANDIC", "MacIceland");
-		oraToJava.put("IS8MACICELANDICS", "MacIceland");
-		oraToJava.put("IS8PC861", "CP861");
-		oraToJava.put("ISO2022-CN", "ISO2022CN_GB");
-		oraToJava.put("ISO2022-JP", "ISO-2022-JP");
-		oraToJava.put("ISO2022-KR", "ISO-2022-KR");
-		oraToJava.put("IW8EBCDIC424", "CP424");
-		oraToJava.put("IW8EBCDIC424S", "CP424");
-		oraToJava.put("IW8ISO8859P8", "ISO-8859-8");
-		oraToJava.put("IW8MACHEBREW", "MacHebrew");
-		oraToJava.put("IW8MACHEBREWS", "MacHebrew");
-		oraToJava.put("IW8MSWIN1255", "WINDOWS-1255");
-		oraToJava.put("IW8PC1507", "CP862");
-		oraToJava.put("JA16EBCDIC930", "CP930");
-		oraToJava.put("JA16EUC", "EUC-JP");
-		oraToJava.put("JA16EUCTILDE", "EUC-JP");
-		oraToJava.put("JA16EUCYEN", "EUC-JP");
-		oraToJava.put("JA16SJIS", "MS932");
-		oraToJava.put("JA16SJISTILDE", "MS932");
-		oraToJava.put("JA16SJISYEN", "MS932");
-		oraToJava.put("JA16VMS", "EUC-JP");
-		oraToJava.put("KO16KSC5601", "MS949");
-		oraToJava.put("KO16MSWIN949", "MS949");
-		oraToJava.put("LT8MSWIN921", "CP921");
-		oraToJava.put("N8PC865", "CP865");
-		oraToJava.put("NEE8ISO8859P4", "ISO-8859-4");
-		oraToJava.put("RU8PC855", "CP855");
-		oraToJava.put("RU8PC866", "CP866");
-		oraToJava.put("S8EBCDIC1143", "Cp1143");
-		oraToJava.put("S8EBCDIC278", "CP278");
-		oraToJava.put("SE8ISO8859P3", "ISO-8859-3");
-		oraToJava.put("TH8MACTHAI", "MacThai");
-		oraToJava.put("TH8MACTHAIS", "MacThai");
-		oraToJava.put("TH8TISASCII", "MS874");
-		oraToJava.put("TH8TISEBCDIC", "CP838");
-		oraToJava.put("TH8TISEBCDICS", "CP838");
-		oraToJava.put("TR8EBCDIC1026", "CP1026");
-		oraToJava.put("TR8EBCDIC1026S", "CP1026");
-		oraToJava.put("TR8MACTURKISH", "MacTurkish");
-		oraToJava.put("TR8MACTURKISHS", "MacTurkish");
-		oraToJava.put("TR8MSWIN1254", "WINDOWS-1254");
-		oraToJava.put("TR8PC857", "CP857");
-		oraToJava.put("UCS2", "UTF-16");
-		oraToJava.put("US7ASCII", "US-ASCII");
-		oraToJava.put("US8PC437", "CP437");
-		oraToJava.put("UTF16", "UTF-16");
-		oraToJava.put("UTF8", "UTF-8");
-		oraToJava.put("VN8MSWIN1258", "WINDOWS-1258");
-		oraToJava.put("WE8EBCDIC1140", "Cp1140");
-		oraToJava.put("WE8EBCDIC1140C", "Cp1140");
-		oraToJava.put("WE8EBCDIC1145", "Cp1145");
-		oraToJava.put("WE8EBCDIC1146", "Cp1146");
-		oraToJava.put("WE8EBCDIC1148", "Cp1148");
-		oraToJava.put("WE8EBCDIC1148C", "Cp1148");
-		oraToJava.put("WE8EBCDIC284", "CP284");
-		oraToJava.put("WE8EBCDIC285", "CP285");
-		oraToJava.put("WE8EBCDIC37", "CP037");
-		oraToJava.put("WE8EBCDIC37C", "CP037");
-		oraToJava.put("WE8EBCDIC500", "CP500");
-		oraToJava.put("WE8EBCDIC500C", "CP500");
-		oraToJava.put("WE8EBCDIC871", "CP871");
-		oraToJava.put("WE8ISO8859P1", "ISO-8859-1");
-		oraToJava.put("WE8ISO8859P15", "ISO-8859-15");
-		oraToJava.put("WE8ISO8859P9", "ISO-8859-9");
-		oraToJava.put("WE8MACROMAN8", "MacRoman");
-		oraToJava.put("WE8MACROMAN8S", "MacRoman");
-		oraToJava.put("WE8MSWIN1252", "WINDOWS-1252");
-		oraToJava.put("WE8PC850", "CP850");
-		oraToJava.put("WE8PC858", "Cp858");
-		oraToJava.put("WE8PC860", "CP860");
-		oraToJava.put("ZHS16CGB231280", "EUC_CN");
-		oraToJava.put("ZHS16GBK", "MS936");
-		oraToJava.put("ZHS32GB18030", "GB18030");
-		oraToJava.put("ZHT16BIG5", "MS950");
-		oraToJava.put("ZHT16HKSCS", "MS950_HKSCS");
-		oraToJava.put("ZHT16MSWIN950", "MS950");
-		oraToJava.put("ZHT32EUC", "EUC-TW");
 	}
 
 }
