a207d1c4c6758441bbb948cdd2ed8ccc3419e600
averemee <averemee@a2.solutions>
2026-08-15 20:38:34 +0200

    add missed header to *DatabaseShipmentAgent.java

--- src/main/java/solutions/a2/cdc/oracle/utils/file/TargetDatabaseShipmentAgent.java
+++ src/main/java/solutions/a2/cdc/oracle/utils/file/TargetDatabaseShipmentAgent.java
@@ -1,3 +1,28 @@
+/**
+ * This file is part of the oracdc project.
+ * Copyright (c) 2018-present, A2 Rešitve d.o.o.
+ * Authors: Aleksei Veremeev
+ *
+ * This program is offered under a commercial and under the AGPL license.
+ * For commercial licensing, contact us at sales@a2.solutions.
+ * For AGPL licensing, see below.
+ *
+ * AGPL licensing:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program; see the file GNU-AGPL-v3.0.adoc.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package solutions.a2.cdc.oracle.utils.file;
 
 import static solutions.a2.cdc.oracle.runtime.config.Parameters.DISTRIBUTED_TARGET_PORT_DEFAULT;
